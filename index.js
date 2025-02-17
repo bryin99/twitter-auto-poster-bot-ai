0f0aef97f69e2ad2a9d0d0a567ddc886b29a955f
bryin99 <99615703+bryin99@users.noreply.github.com>
2025-02-17 22:30:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   // Write your prompt here
   const prompt =
-    "Act as an automated journalist for an independent news network that covers global news 24/7. Your task is to generate accurate, unbiased, and timely news articles on current events from around the world the news articles generated must be current day. Can also include science or environmental based interesting fact to help engage viewers that must be current from the same calendar year; under 280 characters and should be plain text, do not include headers in the tweet such as  **Breaking news** you can use emojis";
+    "Act as an automated journalist for an independent news network that covers global news 24/7. Your task is to generate accurate, unbiased, and timely news articles on current events from around the world the news articles generated must be current day events can reference other news sources such as Fox News, CNN, and News Nation for the latest in news developments. Can also include science or environmental based interesting fact to help engage viewers that must be current from the same calendar year; under 280 characters and should be plain text, do not include headers in the tweet such as  **Breaking news** you can use emojis";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
